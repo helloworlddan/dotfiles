24a5e82d6d7a6f43429819c1f24f9b36f0c28ba8
Stamer, Daniel <daniel.stamer@airberlin.com>
2017-05-10 11:17:57 +0200

    Updated for compatibility on different hosts.
    Signed-off-by: Stamer, Daniel <daniel.stamer@airberlin.com>

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,21 +1,24 @@
 import XMonad
 import XMonad.Config.Desktop
+import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig
 import System.IO
 
-baseConfig = desktopConfig
-
 main = do
-  xmproc <- spawnPipe "urxvtd"
+  xmproc <- spawnPipe "xset +fp /usr/share/fonts/truetype/montecarlo"
   xmproc <- spawnPipe "xmobar"
+  xmproc <- spawnPipe "urxvtd"
   xmproc <- spawnPipe "VBoxClient-all"
-  xmonad $ baseConfig
-    { terminal = "urxvtc"
+  xmonad $ defaultConfig
+    { modMask = mod4Mask
+    , manageHook = manageDocks <+> manageHook defaultConfig
+    , layoutHook = avoidStruts $ layoutHook defaultConfig
+    , terminal = "urxvtc"
     , borderWidth = 2
     , normalBorderColor = "#404040"
     , focusedBorderColor = "#d4dee9"
     }
 	`additionalKeys`
-		[ ((mod1Mask, xK_r ), spawn "dmenu_run -nb '#030405' -nf '#e46c8b' - ")
+		[ ((mod4Mask, xK_r ), spawn "dmenu_run -nb '#030405' -nf '#e46c8b'")
 	]
