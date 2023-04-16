f4afd27dff190ba577e166c9046a0ed9e4c123dc
Daniel Stamer <stamer@google.com>
2023-04-16 09:04:43 +0000

    add nv modules
    Signed-off-by: Daniel Stamer <stamer@google.com>

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -1,3 +1,4 @@
 local M = {}
  M.ui = {theme = 'gruvbox'}
+ M.plugins = "custom.plugins"
  return M
---
+++ .config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,62 @@
+local plugins = {
+
+  {
+    "ray-x/go.nvim",
+    dependencies = {  -- optional packages
+      "ray-x/guihua.lua",
+      "neovim/nvim-lspconfig",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("go").setup()
+    end,
+    event = {"CmdlineEnter"},
+    ft = {"go", 'gomod'},
+    build = ':lua require("go.install").update_all_sync()' -- if you need to install/update all binaries
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gofumpt",
+        "goimports",
+        "goimports-reviser",
+        "golines",
+        "gomodifytags",
+        "gopls",
+        "gotests",
+        "gotestsum",
+
+        "dockerfile-language-server",
+
+        "jsonnet-language-server",
+
+        "ruby-lsp",
+        "rubocop",
+
+        "yaml-language-server",
+        "yamlfmt",
+
+        "terraform-ls",
+        "tflint",
+
+        "css-lsp",
+
+        "lua-language-server",
+        "html-lsp",
+        "prettier",
+        "stylua"
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+     config = function()
+        require "plugins.configs.lspconfig"
+        require "custom.configs.lspconfig"
+     end,
+  },
+
+}
+
+return plugins
