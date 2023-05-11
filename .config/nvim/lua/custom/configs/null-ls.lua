71b6a7d5eb9d462841c615e93dc51779aedab724
Daniel Stamer <stamer@google.com>
2023-05-11 14:31:38 +0000

    fix null-ls augroup init
    Signed-off-by: Daniel Stamer <stamer@google.com>

--- .config/nvim/lua/custom/configs/null-ls.lua
+++ .config/nvim/lua/custom/configs/null-ls.lua
@@ -1,5 +1,5 @@
 local null_ls = require("null-ls")
-local augroup = vim.api.nvim_create_autogroup("LspFormatting", {})
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 
 local opts = {
   sources = {
