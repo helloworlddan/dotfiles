1aa682ab2ede7eda25191518ebb992f3c74b2555
Daniel Stamer <stamer@google.com>
2023-07-19 14:04:23 +0000

    switch to brighter gruvbox light
    Signed-off-by: Daniel Stamer <stamer@google.com>

--- .config/nvim/lua/custom/chadrc.lua
+++ .config/nvim/lua/custom/chadrc.lua
@@ -1,4 +1,15 @@
 local M = {}
- M.ui = {theme = 'gruvbox'}
- M.plugins = "custom.plugins"
- return M
+M.ui = {
+  changed_themes = {
+    gruvbox_light = {
+      base_16 = {
+        base00 = "#fbf1c7",
+      },
+    },
+  },
+  theme = 'gruvbox_light',
+}
+
+M.plugins = "custom.plugins"
+
+return M
