ce2ac53650f1b1d0054bfd352a09ecd217c1eee1
Daniel Stamer <stamer@google.com>
2023-10-05 08:08:11 +0000

    add terraform lsp preload
    Signed-off-by: Daniel Stamer <stamer@google.com>

--- .config/nvim/lua/custom/configs/lspconfig.lua
+++ .config/nvim/lua/custom/configs/lspconfig.lua
@@ -39,3 +39,10 @@ lspconfig.ruby_ls.setup{
   settings = {},
 }
 
+lspconfig.terraformls.setup{}
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+  pattern = {"*.tf", "*.tfvars"},
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+})
