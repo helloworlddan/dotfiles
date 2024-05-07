cd630a18dea40ba8fde6ad1ace8d0b1cb5b8932c
Daniel Stamer <dan@hello-world.sh>
2024-05-07 21:08:53 +0000

    switch to default
    Signed-off-by: Daniel Stamer <dan@hello-world.sh>

--- .themes/polypao_dark/nvim.theme.lua
+++ .themes/polypao_dark/nvim.theme.lua
@@ -6,23 +6,24 @@ return {
 		config = function()
 			require("mini.base16").setup({
 				palette = {
-					base00 = "#1f2328",
-					base01 = "#3e3e3e",
+					base00 = "#292a2d",
+					base01 = "#313131",
 					base02 = "#4e4e4e",
 					base03 = "#5e5e5e",
+
 					base04 = "#FFFFFF",
 					base05 = "#F3F3F3",
 					base06 = "#D8D8D8",
 					base07 = "#b9b9b9",
 					--- NOTE double check
-					base08 = "#639c08",
-					base09 = "#744527",
-					base0A = "#B26A3E",
-					base0B = "#D9006A",
-					base0C = "#00AF8D",
-					base0D = "#A8195F",
-					base0F = "#429484",
-					base0E = "#FF6400",
+					base08 = "#ACE5B3",
+					base09 = "#c6c5fc",
+					base0B = "#9E96Ed",
+					base0C = "#85DDD9",
+					base0D = "#dB8DB3",
+					base0E = "#F4A792",
+					base0F = "#48C1B8",
+					base0A = "#efafD1",
 				},
 			})
 		end,
