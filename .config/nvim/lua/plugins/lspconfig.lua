e3899c0774c0ebd038db7bcc58e424725b9f3396
Daniel Stamer <stamer@google.com>
2024-05-15 14:33:17 +0000

    add lower scale
    Signed-off-by: Daniel Stamer <stamer@google.com>

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -67,7 +67,7 @@ return {
         solargraph = {},
       },
       setup = {
-        gopls = function(_, opts)
+        gopls = function(_, _)
           -- workaround for gopls not supporting semanticTokensProvider
           -- https://github.com/golang/go/issues/54531#issuecomment-1464982242
           LazyVim.lsp.on_attach(function(client, _)
