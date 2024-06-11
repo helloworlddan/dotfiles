50bcdac6be6b5a2ded553264798c7fc9ccd27d02
Daniel Stamer <dan@hello-world.sh>
2024-06-11 10:26:57 +0200

    Add lang support for rb, rs and hs
    Signed-off-by: Daniel Stamer <dan@hello-world.sh>

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -4,9 +4,12 @@ return {
     opts = function(_, opts)
       opts.ensure_installed = opts.ensure_installed or {}
       vim.list_extend(opts.ensure_installed, { "goimports", "gofumpt", "gomodifytags", "impl", "delve" })
+      vim.list_extend(opts.ensure_installed, { "ruby-lsp", "solargraph" })
+      vim.list_extend(opts.ensure_installed, { "rust-analyzer" })
+      vim.list_extend(opts.ensure_installed, { "lua-language-server" })
       vim.list_extend(opts.ensure_installed, { "hadolint" })
+      vim.list_extend(opts.ensure_installed, { "json-lsp" })
       vim.list_extend(opts.ensure_installed, { "markdownlint", "marksman" })
-      vim.list_extend(opts.ensure_installed, { "solargraph" })
     end,
   },
 }
